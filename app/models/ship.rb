45f665507b01a6d75772de78224f98515e3a53b9
RKRedfern <rkredfern@gmail.com>
2020-12-05 09:54:41 -0500

    commit tree bee1e2dff29877de25a006c276ac9f6ab0929d64

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,27 @@
 class Ship
+    attr_reader :name, :type, :booty
+
+    @@ships = []
+
+    # def initialize(name, type, booty)
+    #     @name = name 
+    #     @type = type 
+    #     @booty = booty
+    #     @@ships << self
+    # end
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@ships << self
+    end
+
+    def self.all 
+        @@ships
+    end 
+
+    def self.clear 
+        @@ships = []
+    end
 end
